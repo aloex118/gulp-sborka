65332bc444f97b9b10df624a24f5b45531922636
hanteliai <90987025+aloex118@users.noreply.github.com>
2022-09-18 16:50:00 +0300

    final version

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,24 @@
 const gulp = require('gulp');
 const del = require('del');
 const rename = require('gulp-rename');
+//
 const webp = require('gulp-webp');
 const fonter = require('gulp-fonter');
+const imagemin = require('gulp-imagemin');
 const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass')(require('sass'));
+//
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
-const imagemin = require('gulp-imagemin');
 const size = require('gulp-size');
 const htmlmin = require('gulp-htmlmin');
 const newer = require('gulp-newer');
 const gulppug = require('gulp-pug');
+const webpack = require('webpack-stream');
+//
 const browserSync = require('browser-sync');
 const browsersync = require('browser-sync').create();
 
@@ -128,6 +132,9 @@ function scripts() {
     .pipe(sourcemaps.init())
     .pipe(babel({
         presets: ['@babel/env']
+    }))
+    .pipe(webpack({
+        mode: "development"
     }))
     .pipe(uglify())
     .pipe(concat('main.min.js'))
